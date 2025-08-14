674c01e57f03f0648adfaa9f7c51fc3fa8ed6a6d
Divyansh Sachdev <89719842+divyansh-sachdev@users.noreply.github.com>
2025-08-14 10:00:00 +0530

    Initial commit: MLX90614 infrared body temp and PPG sensor drivers

---
+++ config.example.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define GSR_PIN          34
+#define PULSE_PIN        35
+#define TEMP_I2C_SDA     21
+#define TEMP_I2C_SCL     22
+#define FEVER_TEMP_C     38.0f
+
+#endif
